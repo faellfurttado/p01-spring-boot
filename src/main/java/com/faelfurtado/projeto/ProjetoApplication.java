9b4116a35bdea0ddc1c915410e160153cf07009c
Rafael Furtado <faelfurtado@gmail.com>
2019-04-29 20:36:05 -0300

    Primeira alteração :)

--- src/main/java/com/faelfurtado/projeto/ProjetoApplication.java
+++ src/main/java/com/faelfurtado/projeto/ProjetoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro Projeto com Spring Boot ...");
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
 
