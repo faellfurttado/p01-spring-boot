005d4913fb5464829cc0cf332b211757a1512c90
Rafael Furtado <faelfurtado@gmail.com>
2019-05-22 21:20:58 -0300

    Criação das classes ExceptionHandler e Erro, e rafatoração da classe Resposta

--- src/main/java/dcomp/lpweb/projeto/api/controller/response/Resposta.java
+++ src/main/java/dcomp/lpweb/projeto/api/controller/response/Resposta.java
@@ -1,26 +1,47 @@
 package dcomp.lpweb.projeto.api.controller.response;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Resposta<T> {
 	
 	 private T dados;
-	 private List<String> erros = new ArrayList<>();
+	    private List<Erro> erros = new ArrayList<>();
 
-	 public T getDados() {
-	     return dados;
-	 }
+	    private Resposta() { }
 
-	 public void setDados(T dados) {
-	     this.dados = dados;
-	 }
+	    public static Resposta comDadosDe(Object dados) {
+	        Resposta resposta = new Resposta<>();
+	        resposta.setDados(dados );
+	        return resposta;
+	    }
 
-	 public List<String> getErros() {
-	     return erros;
-	 }
+	    public static Resposta com(List<Erro> erros) {
+	        Resposta resposta = new Resposta<>();
+	        resposta.setErros(erros );
+	        return resposta;
+	    }
 
-	 public void setErros(List<String> erros) {
-	     this.erros = erros;
-	 }
+	    public static Resposta com(Erro erro) {
+	        Resposta resposta = new Resposta<>();
+	        resposta.setErros(Arrays.asList(erro) );
+	        return resposta;
+	    }
+
+	    public void setErros(List<Erro> erros) {
+	        this.erros = erros;
+	    }
+
+	    public T getDados() {
+	        return dados;
+	    }
+
+	    public void setDados(T dados) {
+	        this.dados = dados;
+	    }
+
+	    public List<Erro> getErros() {
+	        return erros;
+	    }
 }
---
+++ src/main/java/dcomp/lpweb/projeto/api/exception/CampeonatoExceptionHandler.java
@@ -0,0 +1,59 @@
+package dcomp.lpweb.projeto.api.exception;
+import dcomp.lpweb.projeto.api.controller.response.Erro;
+import dcomp.lpweb.projeto.api.controller.response.Resposta;
+
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class CampeonatoExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	 @Autowired
+	    private MessageSource messageSource;
+
+	    @Override
+	    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+	                                                                  HttpHeaders headers, HttpStatus status,
+	                                                                  WebRequest request) {
+	        final String mensagem = messageSource.getMessage("parametro.invalido", null, new Locale("pt", "BR"));
+
+	        final Resposta resposta = Resposta.com(new Erro(mensagem, ex.getLocalizedMessage()));
+
+	        return super.handleExceptionInternal(ex, resposta, headers, HttpStatus.BAD_REQUEST, request);
+	    }
+
+
+	    @Override
+	    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+	                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+	        final List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+	        final List<Erro> erros = fieldErrors.stream()
+	                .map(fieldError -> new Erro(fieldError.getDefaultMessage(), ex.getLocalizedMessage()))
+	                .collect(Collectors.toList());
+
+	        return super.handleExceptionInternal(ex, Resposta.com(erros), headers, HttpStatus.BAD_REQUEST, request);
+	    }
+
+	    @ExceptionHandler(EmptyResultDataAccessException.class)
+	    public Resposta<Erro> handleEmptyResultDataAccess(EmptyResultDataAccessException ex) {
+
+	        String mensagem = String.format("Recurso nao encontrado, Quantidade Esperada %d, Quantidade Encontrada %d ",
+	                                         ex.getExpectedSize(), ex.getActualSize());
+	        return Resposta.com( new Erro(mensagem, ex.getMostSpecificCause().toString() ) );
+	    }
+}
